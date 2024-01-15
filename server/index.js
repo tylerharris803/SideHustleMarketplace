a77e877e577edd4c522eb31587a92495b15874ff
Brandon Jacobs <jacobs27@student.byu.edu>
2024-01-14 17:06:50 -0700

    Serve production build from express server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 import dotenv from 'dotenv';
-import express, {response} from 'express';
+import path from 'path';
+import express from 'express';
 import {prisma} from "./db/prisma.js";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
     res.json({ message: "I like Brandon's bum" });
@@ -27,6 +36,11 @@ app.get("/getExerciseNames", async (req, res) => {
     const exerciseNames = exercises.map((exercise) => exercise.name);
 
     res.send(exerciseNames);
+});
+
+// This serves the React app in production
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 

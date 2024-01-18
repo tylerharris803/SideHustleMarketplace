16665d693b176caddce0859ce22d481a8c11bf9a
Lauren Stolk <laurenstolk@outlook.com>
2024-01-18 13:05:18 -0700

    updated prisma schema

--- server/routes/api/exercise/createExercise.js
+++ server/routes/api/exercise/createExercise.js
@@ -3,9 +3,11 @@ import {prisma} from "../../../db/prisma.js";
 export default async function (req, res) {
     const newExercise = await prisma.exercise.create({
         data: {
+            id: 4,
             name: "bench press",
-            category: "strength",
-            description: "lift the bar"
+            category: 1,
+            description: "lift the bar",
+            media: "blahblahblah"
         }
     });
     res.send(`${newExercise.name} was created in the db`);
--- server/routes/api/exercise/getExerciseNames.js
+++ server/routes/api/exercise/getExerciseNames.js
@@ -1,6 +1,6 @@
 import {prisma} from "../../../db/prisma.js";
 
-export default async function (req,res) {
+export default async function (req, res) {
     const exercises = await prisma.exercise.findMany();
     const exerciseNames = exercises.map((exercise) => exercise.name);
 

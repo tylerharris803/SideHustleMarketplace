9b8e5c9747dea2f7fac9a1198763e8961c9f5fce
Lauren Stolk <laurenstolk@outlook.com>
2024-02-06 11:29:13 -0700

    complete workout page

---
+++ client/src/layouts/completeworkout/components/CompleteWorkout/index.js
@@ -0,0 +1,26 @@
+// @mui material components
+import Card from "@mui/material/Card";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+
+import { FormControl, InputLabel, Select } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+
+import React, { useState } from "react";
+
+// Add supabase connection
+import { supabase } from "../../../../supabaseClient";
+import { useNavigate } from "react-router-dom";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function CompleteWorkout() {
+
+
+}
+export default CompleteWorkout;

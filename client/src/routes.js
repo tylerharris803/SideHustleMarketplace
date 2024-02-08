e3cfb567aacfd10049e16e8c81c0fb23c2169d86
Lauren Stolk <laurenstolk@outlook.com>
2024-02-08 16:28:56 -0700

    complete workout is working!

--- client/src/routes.js
+++ client/src/routes.js
@@ -61,11 +61,10 @@ import PeopleIcon from "@mui/icons-material/People";
 import DirectionsRunIcon from "@mui/icons-material/DirectionsRun";
 import CollectionsBookmarkIcon from "@mui/icons-material/CollectionsBookmark";
 
-import AddNewTeam from "./layouts/addteam/components/AddTeam";
 import Addnewteam from "./layouts/addteam";
-//import AddWorkout from "layouts/addworkout/components/AddWorkout";
 import AddWorkout from "./layouts/addworkout";
 import Addnewwellness from "./layouts/addwellness";
+import CompleteWorkout from "./layouts/completeworkout";
 
 const routes = [
   {
@@ -117,7 +116,6 @@ const routes = [
     route: "/addworkout",
     component: <AddWorkout />,
   },
-
   {
     type: "collapse",
     name: "Add New Team",
@@ -126,7 +124,6 @@ const routes = [
     route: "/addteam",
     component: <Addnewteam />,
   },
-
   {
     type: "collapse",
     name: "Add New Wellness",
@@ -161,6 +158,14 @@ const routes = [
   },
   {
     type: "collapse",
+    name: "Complete Workout",
+    key: "completeworkout",
+    icon: <Icon fontSize="small">person</Icon>,
+    route: "/completeworkout",
+    component: <CompleteWorkout />,
+  },
+  {
+    type: "collapse",
     name: "Sign In",
     key: "sign-in",
     icon: <Icon fontSize="small">login</Icon>,
@@ -185,6 +190,7 @@ const routes = [
   },
   {
     type: "collapse",
+    name: "Coach Info",
     key: "coachinfo",
     icon: <Icon fontSize="small"></Icon>,
     route: "/authentication/coachinfo",
@@ -192,6 +198,7 @@ const routes = [
   },
   {
     type: "collapse",
+    name: "Team Info",
     key: "teaminfo",
     icon: <Icon fontSize="small"></Icon>,
     route: "/authentication/teaminfo",
@@ -199,6 +206,7 @@ const routes = [
   },
   {
     type: "collapse",
+    name: "Wellness Setup",
     key: "wellness-setup",
     icon: <Icon fontSize="small"></Icon>,
     route: "/authentication/wellness-setup",
@@ -206,6 +214,7 @@ const routes = [
   },
   {
     type: "collapse",
+    name: "Summary",
     key: "summary",
     icon: <Icon fontSize="small"></Icon>,
     route: "/authentication/summary",
@@ -213,6 +222,7 @@ const routes = [
   },
   {
     type: "collapse",
+    name: "Sign Up",
     key: "sign-up",
     icon: <Icon fontSize="small"></Icon>,
     route: "/authentication/sign-up",
